731ac71b86abbd42a335afe34b8c764389ba4c0e
VladimirSolovov <vovan73ul@gmail.com>
2018-11-29 14:20:53 +0400

    write to json

--- json/input.php
+++ json/input.php
@@ -1,2 +1,7 @@
-<?php
+﻿<?php
+$jsonarr = $_POST;
+$delsymbol = [' ', '<', '>', '\r', '\n'];
+$nullsymbol = '';
+$jsonarr = str_replace($delsymbol, $nullsymbol, $jsonarr);
+file_put_contents('userdata.json', json_encode($jsonarr));
 ?>
