631d06f5fbfa659c36b1d94380aff54df0f283c8
VladimirSolovov <vovan73ul@gmail.com>
2018-12-05 18:10:21 +0400

    add test system

--- tests/view.php
+++ tests/view.php
@@ -2,5 +2,15 @@
 include '/templates/header.php';
 $file = file_get_contents(__DIR__.'/tests/'.$_GET['namefile']);
 $data = json_decode($file, true);
+foreach ($data[0] as $quest) {
+	?><pre><?
+	 echo $quest['textQwestion']?><br><?;
+	?><pre><?
+	foreach ($quest['answer'] as $k => $variant) {
+?>
+    <input type="radio" name="answer" value="answer" ><?echo $variant;?><br/>
+<?
+	}
+}
 include '/templates/footer.php';
 ?>
