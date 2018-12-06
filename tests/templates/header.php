51fd181db6fe9cee0637c521e1f906ca93d16f90
VladimirSolovov <vovan73ul@gmail.com>
2018-12-06 12:39:59 +0400

    Добавлена проверка результатов

--- tests/templates/header.php
+++ tests/templates/header.php
@@ -4,7 +4,7 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-<title>Авторизация</title>
+<title>Тестирование</title>
     <meta charset=UTF-8"/>
     <link href="css/style.css" rel="stylesheet" type="text/css" />
 </head>
